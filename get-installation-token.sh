dbf92f44feabb7b2aa5c35284410e828cafa0ea4
Christer Edvartsen <christer.edvartsen@nav.no>
2020-06-25 14:47:13 +0200

    Prefix with correct path

--- get-installation-token.sh
+++ get-installation-token.sh
@@ -2,7 +2,7 @@
 export PRIVATE_KEY=$1
 export APP_ID=$2
 
-jwt=$(ruby ./generate-jwt.rb)
+jwt=$(ruby $(dirname $0)/generate-jwt.rb)
 installation_id=$(curl -s \
 -H "Authorization: Bearer ${jwt}" \
 -H "Accept: application/vnd.github.machine-man-preview+json" \
