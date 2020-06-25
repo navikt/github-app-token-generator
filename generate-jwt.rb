37a69905c3dccd681d9491240ec95ec58f91c32d
Christer Edvartsen <christer.edvartsen@nav.no>
2020-06-25 15:39:17 +0200

    Add some basic error handling

--- generate-jwt.rb
+++ generate-jwt.rb
@@ -1,8 +1,11 @@
 require 'openssl'
 require 'jwt'
 
+private_key = ENV.fetch('PRIVATE_KEY')
+app_id = ENV.fetch('APP_ID')
+
 puts JWT.encode({
   iat: Time.now.to_i,
   exp: Time.now.to_i + (10 * 60),
-  iss: ENV['APP_ID']
-}, OpenSSL::PKey::RSA.new(ENV['PRIVATE_KEY']), 'RS256')
+  iss: app_id
+}, OpenSSL::PKey::RSA.new(private_key), 'RS256')
--- get-installation-token.sh
+++ get-installation-token.sh
@@ -1,14 +1,27 @@
-#!/bin/sh -l
-export PRIVATE_KEY=$1
-export APP_ID=$2
+#!/bin/sh -e
+export PRIVATE_KEY=${1:?Usage: ${0} <private-key> <app-id>}
+export APP_ID=${2:?Usage: ${0} <private-key> <app-id>}
+repo=${GITHUB_REPOSITORY:?Missing required GITHUB_REPOSITORY environment variable}
 
 jwt=$(ruby $(dirname $0)/generate-jwt.rb)
 installation_id=$(curl -s \
 -H "Authorization: Bearer ${jwt}" \
 -H "Accept: application/vnd.github.machine-man-preview+json" \
-https://api.github.com/repos/${GITHUB_REPOSITORY}/installation | jq -r .id)
+https://api.github.com/repos/${repo}/installation | jq -r .id)
 
-echo ::set-output name=token::$(curl -s -X POST \
+if [ "$installation_id" = "null" ]; then
+  echo "Unable to get installation ID. Is the GitHub App installed on ${repo}?"
+  exit 1
+fi
+
+token=$(curl -s -X POST \
 -H "Authorization: Bearer ${jwt}" \
 -H "Accept: application/vnd.github.machine-man-preview+json" \
 https://api.github.com/app/installations/${installation_id}/access_tokens | jq -r .token)
+
+if [ "$token" = "null" ]; then
+  echo "Unable to generate installation token"
+  exit 1
+fi
+
+echo ::set-output name=token::${token}
