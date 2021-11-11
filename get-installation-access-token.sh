8ce38b883a632c7d99722f6bc36e42f66e9237e9
Kyrre Havik <kyrremann@gmail.com>
2021-11-11 14:27:41 +0100

    feat: echo melding hvis vi mangler id

--- get-installation-access-token.sh
+++ get-installation-access-token.sh
@@ -6,14 +6,13 @@ repo=${GITHUB_REPOSITORY:?Missing required GITHUB_REPOSITORY environment variabl
 [[ ! -z "$INPUT_REPO" ]] && repo=$INPUT_REPO
 
 jwt=$(ruby $(dirname $0)/generate-jwt.rb)
-installation_id=$(curl -s \
--H "Authorization: Bearer ${jwt}" \
--H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/${repo}/installation | jq -r .id)
+response=$(curl -s -H "Authorization: Bearer ${jwt}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${repo}/installation)
+installation_id=$(echo $response | jq -r .id)
 
 if [ "$installation_id" = "null" ]; then
-  echo "Unable to get installation ID. Is the GitHub App installed on ${repo}?"
-  exit 1
+    echo "Unable to get installation ID. Is the GitHub App installed on ${repo}?"
+    echo $(echo $response | jq -r .message)
+    exit 1
 fi
 
 token=$(curl -s -X POST \
